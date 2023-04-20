c9c030d4947b9c2a6805ad8797d7291979cdd2de
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2023-04-20 00:30:13 -0300

    templates: add c and c++.

---
+++ template.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+/* calculation */
+int solve (int a)
+{
+  return a * 2;
+}
+
+int main(void)
+{
+  /* output rules */
+  // precision: 3
+
+  /* input */
+  int a;
+  scanf("%d", &a);
+
+  /* output */
+  printf("%d\n", solve(a));
+}
---
+++ template.c++
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* calculation */
+int solve (int a)
+{
+  return a * 2;
+}
+
+int main()
+{
+  /* output rules */
+  cout << fixed << setprecision(3);
+
+  /* input */
+  int a;
+  cin >> a;
+
+  /* output */
+  cout << solve(a) << '\n';
+}
