5735102943b71aef51deb962548ba447e6efa34c
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2023-04-21 05:42:38 -0300

    apc: maior numero2

---
+++ apc_maior_numero2/source.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+/* calculation */
+int solve (int a)
+{
+  int maior;
+  scanf("%d", &maior);
+
+  while (a--)
+  {
+    int n;
+    scanf("%d", &n);
+    if (n > maior)
+      maior = n;
+  }
+  return maior;
+}
+
+int main(void)
+{
+  /* input */
+  int a;
+  scanf("%d", &a);
+
+  /* output */
+  printf("%d\n", solve(a));
+}
