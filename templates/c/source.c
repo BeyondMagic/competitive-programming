eb79f6a749ec4c54ace68f4a8ddc5076588714b0
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2023-05-01 01:41:30 -0300

    templates: add macros to deal with floating types

--- templates/c/source.c
+++ templates/c/source.c
@@ -4,8 +4,25 @@
 #include <math.h>
 #include <stdbool.h>
 
-#define MAX(x, y) (((x) > (y)) ? (x) : (y))
-#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+/* */
+#define CLOSE_EQUAL_DECIMAL(a, b, epsilon) (fabs((a) - (b)) <= ( (fabs(a) < fabs(b) ? fabs(b) : fabs(a)) * (epsilon)))
+#define ESSENTIALLY_EQUAL_DECIMAL(a, b, epsilon) (fabs((a) - (b)) <= ( (fabs(a) > fabs(b) ? fabs(b) : fabs(a)) * (epsilon)))
+
+/* */
+#define GREATER_DECIMAL(a, b, epsilon) ((a) - (b)) > ( (fabs(a) < fabs(b) ? fabs(b) : fabs(a)) * (epsilon))
+#define LESS_DECIMAL(a, b, epsilon) (((b) - (a)) > ( (fabs(a) < fabs(b) ? fabs(b) : fabs(a)) * (epsilon)))
+
+/* Maximum number from two integers. */
+#define MAX_INT(a, b) (((a) > (b)) ? (a) : (b))
+#define MIN_INT(a, b) (((a) < (b)) ? (a) : (b))
+
+/* Maximum number from two doubles. */
+#define MAX_DECIMAL(a, b, epsilon) (GREATER_DECIMAL(a, b, epsilon) ? (a) : (b)))
+#define MIN_DECIMAL(a, b, epsilon) (LESS_DECIMAL(a, b, epsilon) ? (a) : (b)))
+
+/* Basic mathematical logics. */
+#define IS_EVEN(integer) ((long long) (integer) % 2)
+#define IS_ODD(integer) (!(IS_EVEN(integer)))
 
 /* calculation */
 int solve (int a)
