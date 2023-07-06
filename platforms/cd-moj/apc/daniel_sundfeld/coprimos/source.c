4509bc6103b9ee3e2c874f5c547e99bbf8d8ba9c
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2023-07-06 00:51:32 -0300

    apc: add coprimos solution

---
+++ platforms/cd-moj/apc/daniel_sundfeld/coprimos/source.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+int main(void)
+{
+  /* input */
+  int a;
+  scanf("%d", &a);
+
+  /* output */
+  printf("%d\n", a);
+}
