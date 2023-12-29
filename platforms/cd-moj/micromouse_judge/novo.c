7942a2abef6037f267fbd1316a854720b63915d3
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2023-12-28 22:28:03 -0300

    rewriting micromouse 2

--- platforms/cd-moj/micromouse_judge/novo.c
+++ platforms/cd-moj/micromouse_judge/novo.c
@@ -39,14 +39,15 @@ enum Room_State
 	blank,
 	cheese,
 	visited,
+	unknown,
 };
 
-typedef struct room
+typedef struct Room
 {
 	enum Room_State state;
-	struct room *adjacents;
+	struct Room **adjacents;
 	struct Position position;
-} room;
+} Room;
 
 // -------------------------------------------------------
 // Player implementation
@@ -129,6 +130,7 @@ void stack_pop(struct Stack *stack)
 	struct Node *temp = stack->top;
 	stack->top = stack->top->next;
 	stack->top->prev = NULL;
+	free(temp->data);
 	free(temp);
 	--stack->size;
 }
@@ -197,8 +199,13 @@ void queue_pop(struct Queue *queue)
 	assert(queue and queue->front and queue->back);
 	struct Node *temp = queue->front;
 	queue->front = queue->front->next;
-	queue->front->prev = NULL;
+	if (queue->front)
+		queue->front->prev = NULL;
+	else
+		queue->back = NULL;
+	free(temp->data);
 	free(temp);
+	--queue->size;
 }
 
 void queue_free(struct Queue *queue)
@@ -225,6 +232,109 @@ struct List *list_initialise()
 bool list_empty(struct List *list)
 {
 	return list->size;
+}
+
+void list_insert_front(struct List *list, void *data)
+{
+	assert(list and data);
+	struct Node *temp = malloc(sizeof(struct Node));
+
+	temp->data = data;
+	temp->prev = NULL;
+	temp->next = list->front;
+
+	if (list->front)
+		list->front->prev = temp;
+
+	list->front = temp;
+
+	if (not list->back)
+		list->back = temp;
+
+	++list->size;
+}
+
+void list_insert_back(struct List *list, void *data)
+{
+	assert(list and data);
+	struct Node *temp = malloc(sizeof(struct Node));
+
+	temp->data = data;
+	temp->prev = list->back;
+	temp->next = NULL;
+
+	if (list->back)
+		list->back->next = temp;
+
+	list->back = temp;
+
+	if (not list->front)
+		list->front = temp;
+
+	++list->size;
+}
+
+void list_pop_front(struct List *list)
+{
+	assert(list and list->front and list->back);
+	struct Node *temp = list->front;
+	list->front = list->front->next;
+	list->front->prev = NULL;
+	free(temp->data);
+	free(temp);
+}
+
+void list_pop_back(struct List *list)
+{
+	assert(list and list->front and list->back);
+	struct Node *temp = list->back;
+	list->back = list->back->prev;
+	list->back->next = NULL;
+	free(temp->data);
+	free(temp);
+}
+
+struct List *positions_passed, *possible_cheese;
+
+// --------------------------
+// Debug functions.
+// --------------------------
+
+char *room_state_string(enum Room_State state)
+{
+	static char *s_wall = "wall",
+				*s_cheese = "cheese",
+				*s_blank = "blank",
+				*s_visited = "visited",
+				*s_unknown = "unknown";
+
+	switch (state)
+	{
+		case wall:
+			return s_wall;
+		case cheese:
+			return s_cheese;
+		case blank:
+			return s_blank;
+		case visited:
+			return s_visited;
+		case unknown:
+			return s_unknown;
+	}
+}
+
+void print_room_situation(Room *u)
+{
+	debug("This room u at (%d, %d)", u->position.x, u->position.y);
+	debug("Its children are:");
+	if (u->adjacents[up])
+		debug("\t\tu->adjacents[up]->state: %s", room_state_string(u->adjacents[up]->state));
+	if (u->adjacents[right])
+		debug("\t\tu->adjacents[right]->state: %s", room_state_string(u->adjacents[right]->state));
+	if (u->adjacents[down])
+		debug("\t\tu->adjacents[down]->state: %s", room_state_string(u->adjacents[down]->state));
+	if (u->adjacents[left])
+		debug("\t\tu->adjacents[left]->state: %s", room_state_string(u->adjacents[left]->state));
 }
 
 // -------------------------------------------------------
@@ -267,6 +377,106 @@ int send_command(enum Commands id)
 
 	return result;
 };
+
+void destroy_grid(Room *u)
+{
+	struct Queue *Q = queue_initialise();
+	queue_push(Q, u);
+
+	while (not queue_empty(Q))
+	{
+		u = queue_front(Q);
+		queue_pop(Q);
+
+		for (enum Entity_State direction = up;;)
+		{
+			if (u->adjacents[direction])
+				u->adjacents[direction]->adjacents[(direction + 2) % 4] = NULL,
+				queue_push(Q, u->adjacents[direction]);
+
+			if (++direction == up)
+				break;
+		}
+
+		free(u);
+	}
+
+	queue_free(Q);
+}
+
+Room *create_room(enum Room_State state, Room *r_up, Room *r_right, Room *r_down, Room *r_left, int x, int y)
+{
+	Room *u = malloc(sizeof(Room));
+
+	u->state = state;
+	u->adjacents = malloc(sizeof(Room *) * 4);
+	u->adjacents[up] = r_up;
+	u->adjacents[right] = r_right;
+	u->adjacents[down] = r_down;
+	u->adjacents[left] = r_left;
+	u->position.x = x;
+	u->position.y = y;
+
+	return u;
+}
+
+void read_sensor(Room *u, int result)
+{
+	if (not u->adjacents[up])
+		u->adjacents[up] = create_room(unknown, NULL, NULL, u, NULL, u->position.x, u->position.y + 1);
+
+	if (not u->adjacents[right])
+		u->adjacents[right] = create_room(unknown, NULL, NULL, NULL, u, u->position.x + 1, u->position.y);
+
+	if (not u->adjacents[down])
+		u->adjacents[down] = create_room(unknown, u, NULL, NULL, NULL, u->position.x, u->position.y - 1);
+
+	if (not u->adjacents[left])
+		u->adjacents[left] = create_room(unknown, NULL, u, NULL, NULL, u->position.x - 1, u->position.y);
+
+	int i = 1;
+	enum Entity_State orig = player.state;
+	do {
+		if (u->adjacents[player.state]->state == unknown)
+			u->adjacents[player.state]->state = result & i ? blank : wall;
+		i <<= 1;
+	} while (++player.state != orig);
+	player.state = orig;
+}
+
+void change_direction(enum Entity_State direction)
+{
+	if (player.state == direction)
+		return;
+
+	// Opposite side.
+	if (
+			(player.state == down and direction == up) or
+			(player.state == up and direction == down) or
+			(player.state == left and direction == right) or
+			(player.state == right and direction == left)
+		)
+		send_command(rotate_right),
+		send_command(rotate_right);
+
+	else if (
+		(player.state == up and direction == left) or
+		(player.state == left and direction == down) or
+		(player.state == right and direction == up) or
+		(player.state == down and direction == right)
+		)
+		send_command(rotate_left);
+
+	else if (
+		(player.state == up and direction == right) or
+		(player.state == right and direction == down) or
+		(player.state == down and direction == left) or
+		(player.state == left and direction == up)
+		)
+		send_command(rotate_right);
+
+	player.state = direction;
+}
 
 int main (void)
 {
