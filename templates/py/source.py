3f72a4b137bc30f484f7f0697d0fa5eafcc37612
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2024-09-16 22:16:21 -0300

    templates(py): add Point and Segment classes

--- templates/py/source.py
+++ templates/py/source.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python
 
-def read_list():
-    return [int(i) for i in input().split]
-
 from math import *
 
-t = int(input())
-print(t ** 2)
+# Snippets:
+def read_list():
+    return [int(i) for i in input().split()]
+
+# Computational Geometry:
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Segment:
+    def __init__(self, P: Point, Q: Point):
+        self.P = P
+        self.Q = Q
+
+    def distance(self, R: Point):
+        return (self.P.x * self.Q.y + self.P.y * R.x + self.Q.x * R.y) - (R.x * self.Q.y + R.y * self.P.x + self.Q.x * self.P.y)
+
+# Main program:
+a, b = read_list()
+print(a + b)
