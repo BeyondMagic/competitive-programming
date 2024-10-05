51cdb85e39eb86f626edcc14600b372348883527
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2024-10-05 08:54:08 -0300

    py: add more methods for geometry classes

--- templates/py/source.py
+++ templates/py/source.py
@@ -12,6 +12,23 @@ class Point:
         self.x = x
         self.y = y
 
+    def distance(self, P):
+        return hypot(self.x - P.x, self.y - P.y)
+
+class Line:
+    def __init__(self, P: Point, Q: Point):
+        self.a = P.y - Q.y
+        self.b = Q.x - P.x
+        self.c = P.x * Q.y - Q.x * P.y
+
+    def closest(self, P: Point):
+        den = (self.a ** 2 + self.b ** 2)
+
+        x = (self.b * (self.b * P.x - self.a * P.y) - self.a * self.c) / den
+        y = (self.a * (-self.b * P.x + self.a * P.y) - self.b * self.c) / den
+
+        return Point(x, y)
+
 class Segment:
     def __init__(self, P: Point, Q: Point):
         self.P = P
@@ -19,6 +36,21 @@ class Segment:
 
     def distance(self, R: Point):
         return (self.P.x * self.Q.y + self.P.y * R.x + self.Q.x * R.y) - (R.x * self.Q.y + R.y * self.P.x + self.Q.x * self.P.y)
+
+    def contains(self, R: Point):
+        return self.distance(R) == 0
+
+    def closest(self, R: Point):
+        A = Line(self.P, self.Q).closest(R)
+
+        if self.contains(A):
+            return A
+
+        elif self.P.distance(R) <= self.Q.distance(R):
+            return self.P
+
+        else:
+            return self.Q
 
 # Main program:
 a, b = read_list()
