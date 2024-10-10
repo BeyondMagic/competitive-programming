5c0bbb31e316dd66e1a2d747a37124de52c8116e
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2024-10-10 07:12:40 -0300

    feat(debug): add c debug file

---
+++ debug/debug.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// void __print(long long x) { cerr << x; }
+// void __print(unsigned x) { cerr << x; }
+// void __print(unsigned long x) { cerr << x; }
+// void __print(unsigned long long x) { cerr << x; }
+// void __print(float x) { cerr << x; }
+// void __print(double x) { cerr << x; }
+// void __print(long double x) { cerr << x; }
+// void __print(char x) { cerr << '\'' << x << '\''; }
+// void __print(const char *x) { cerr << '\"' << x << '\"'; }
+// void __print(const string &x) { cerr << '\"' << x << '\"'; }
+// void __print(bool x) { cerr << (x ? "true" : "false"); }
