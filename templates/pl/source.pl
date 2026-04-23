ab9361b4a063e8a5f434537aca91bccfd78dcf50
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2026-04-23 20:58:15 -0300

    feat(pl): refactor input handling and simplify solve function

--- templates/pl/source.pl
+++ templates/pl/source.pl
@@ -2,29 +2,20 @@
 
 main :-
     % -----------------------------------
-    read_line_to_string(user_input, Line),
-    % split_string(+String, +SepChars, +PadChars, -SubStrings).
-    split_string(Line, " ", " ", [S1, S2, S3]),
-    % Atom to number
-    atom_number(S1, N1),
-    atom_number(S2, N2),
-    atom_number(S3, N3),
+    read_line_to_string(user_input, Line1), split_string(Line1, " ", " ", [S1]),
+    read_line_to_string(user_input, Line2), split_string(Line2, " ", " ", [S2]),
+    atom_number(S1, E),
+    atom_number(S2, D),
     % -----------------------------------
-    solve(N1, N2, N3, X),
+    solve(E, D, X),
     writeln(X),
     halt.
 
 :- initialization(main).
-:- use_module(library(arithmetic)).
-:- arithmetic_function(my_min/2).
-:- op(800, yfx, my_min).
 
-X = A my_min B :- ((A =< B) -> (X = A) ; (X = B)), !.
-A my_min B = X :- ((A =< B) -> (X = A) ; (X = B)), !.
-
-my_min(A, B, X) :-
-	A =< B -> (X = A) ; (X = B).
-
-solve(A, B, C, X) :-
-	X = min(A // 2, min(B // 3, C // 5)).
-
+solve(E, D, X) :-
+    E > D -> (
+        X is E + D
+    ) ; (
+        X is 2 * (D - E)
+    ).
