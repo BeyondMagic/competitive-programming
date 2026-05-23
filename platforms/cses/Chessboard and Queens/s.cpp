91798a5c71894562879de5efed2b045f5ce978ae
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2026-05-23 11:13:30 -0300

    feat(codeforces): add cpp solutions to 'Chessboard and Queens' cses

--- platforms/cses/Chessboard and Queens/s.cpp
+++ platforms/cses/Chessboard and Queens/s.cpp
@@ -262,3 +262,44 @@ bool is_less_greater_or_equal(T a, T b, T epsilon = std::numeric_limits<T>::epsi
 {
 	return (b < a) || are_nearly_equal(b, a, epsilon);
 }
+
+using namespace std;
+
+vector<string> G;
+
+array<bool, 16> ldig, rdig, column;
+
+int k = 0;
+
+auto solve (int R = 0)
+{
+	if (R == 8)
+	{
+		++k;
+		return;
+	}
+
+	for (int c = 0; c < 8; ++c)
+	{
+		auto l = R - c + 7;
+		auto r = R + c;
+
+		if (G[R][c] == '*' or column[c] or ldig[l] or rdig[r])
+			continue;
+
+		column[c] = ldig[l] = rdig[r] = true;
+		solve(R + 1);
+		column[c] = ldig[l] = rdig[r] = false;
+	}
+}
+
+int main()
+{
+	fast_io();
+	G.resize(8);
+	for (auto i = 0; i < 8; ++i)
+		cin >> G.at(i);
+	solve();
+	cout << k << endl;
+}
+
